1a31d831c6a41466d13b8aa68b4759b076530fde
jswang0037 <jswang0037@gmail.com>
2022-11-22 20:11:33 +0800

    update readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'app-money-log-jswang',
+    appId: '1:475065358399:web:b2880fd16b5ba223d9008a',
+    storageBucket: 'app-money-log-jswang.appspot.com',
+    apiKey: 'AIzaSyDN5MvUo9M0o-_EE_PVZ-g3CSVMDtFUSMw',
+    authDomain: 'app-money-log-jswang.firebaseapp.com',
+    messagingSenderId: '475065358399',
+  },
   production: false
 };
 
